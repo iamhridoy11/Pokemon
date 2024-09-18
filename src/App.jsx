4879c69482a6839a80e622c46fddf9747a44972d
iamhridoy11 <m_rahman_hridoy@yahoo.com>
2024-09-18 18:05:29 -0400

    push V1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Pokegame from "./Pokegame";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Pokegame />
+      </div>
+    </>
+  );
+}
+
+export default App;
